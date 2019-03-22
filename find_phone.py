c37d46b465235780d0576b5e0a61f8d3c1bc979c
karanchahal <karanbleep@gmail.com>
2019-03-22 13:27:35 +0530

    Some error fixes

--- find_phone.py
+++ find_phone.py
@@ -22,6 +22,7 @@ import mrcnn.model as modellib
 from mrcnn import visualize
 from mrcnn.model import log
 
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
 # For inference results
 def returnCenterPoint(bbox):
@@ -34,7 +35,42 @@ def predict(r):
     return returnCenterPoint([x1,y1,x2,y2])
 
 
-class InferenceConfig(BDDConfig):
+class PhoneConfig(Config):
+    """Configuration for training on the objects of BDD dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "bdd"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 2  # background + 1 for only traffic lights
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 32
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 100
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 5
+
+
+class InferenceConfig(PhoneConfig):
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
     DETECTION_MIN_CONFIDENCE = 0.9
